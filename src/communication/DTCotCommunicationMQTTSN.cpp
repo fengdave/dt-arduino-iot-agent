7da744bc0db7b0a4cbb20a951462b6d975c7e673
James Hudson <psytrakked@gmail.com>
2018-01-25 13:12:26 +0100

    Added headers

--- src/communication/DTCotCommunicationMQTTSN.cpp
+++ src/communication/DTCotCommunicationMQTTSN.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file {{file}}
+ * @description Basic setup and protocol to interact 
+ * with the Deutsche Telekom Cloud of things MQTT-SN connector.
+ * @author Andreas Krause
+ * @copyright (C) 2018 mm1 Technology GmbH - all rights reserved. 
+ * @licence MIT licence
+ * 
+ * Find out more about mm1 Technology:
+ * Company: http://mm1-technology.de/
+ * GitHub:  https://github.com/mm1technology/
+ */
+
 #include "DTCOTSetup.h"
 
 #if CONN_TYPE == NB_IOT
--- src/devices/DTCoTDeviceEthernet.h
+++ src/devices/DTCoTDeviceEthernet.h
@@ -1,3 +1,16 @@
+/**
+ * @file {{file}}
+ * @description Basic setup and protocol to interact 
+ * with the Deutsche Telekom Cloud of things MQTT-SN connector.
+ * @author Andreas Krause
+ * @copyright (C) 2018 mm1 Technology GmbH - all rights reserved. 
+ * @licence MIT licence
+ * 
+ * Find out more about mm1 Technology:
+ * Company: http://mm1-technology.de/
+ * GitHub:  https://github.com/mm1technology/
+ */
+
 #ifndef DT_COT_DEVICE_ETHERNET_HEADER_FILE
 #define DT_COT_DEVICE_ETHERNET_HEADER_FILE
 
